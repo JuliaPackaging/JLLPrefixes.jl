77282ade764685274e681df5b964318033f76d0e
Elliot Saba <staticfloat@gmail.com>
2023-02-05 10:25:38 -0800

    Fix tests on aarch64-apple-darwin
    We dodge a few Pkg issues here, and improve our testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -75,8 +75,23 @@ using JLLPrefixes: PkgSpec
         @test sort([p.name for p in keys(artifact_paths)]) == ["Bzip2_jll", "Zstd_jll"]
     end
 
-    # Test stdlibs across versions
-    for (GMP_soversion, julia_version) in (("10.3.2", v"1.5"), ("10.4.0", v"1.6"), ("10.4.1", v"1.7"))
+    # Test stdlibs across versions.  Note that `GMP_jll` was _not_ a standard library in v1.5,
+    # it _is_ a standard library in v1.6 and v1.7.  Further note that beacuse it had no valid
+    # mapping for aarch64-apple-darwin in Julia v1.5, these tests fail for that version on that
+    # platform; we skip that version in thiat case.  I consider this something of a Pkg bug,
+    # since we are resolving for x86_64-linux-gnu
+    GMP_JULIA_VERSIONS = [
+        ("10.3.2", v"1.5"),
+        ("10.4.0", v"1.6"),
+        ("10.4.1", v"1.7"),
+        ("10.4.1", v"1.8"),
+    ]
+    if platforms_match(HostPlatform(), Platform("aarch64", "macos"))
+        filter!(GMP_JULIA_VERSIONS) do (gmp_version, julia_version)
+            return julia_version >= v"1.6"
+        end
+    end
+    for (GMP_soversion, julia_version) in GMP_JULIA_VERSIONS
         @testset "GMP_jll (Julia $(julia_version))" begin
             artifact_paths = collect_artifact_paths(["GMP_jll"]; platform=Platform("x86_64", "linux"; julia_version))
             @test length(artifact_paths) == 1
@@ -87,22 +102,22 @@ using JLLPrefixes: PkgSpec
 
     # Test "impossible" situations via `julia_version == nothing`
     @testset "Impossible Constraints" begin
-        # We can't naively install GMP v6.1.2 and MPFR v4.1.0, because those are
+        # We can't naively install OpenBLAS v0.3.13 and LBT v5.1.1, because those are
         # from conflicting Julia versions, and the Pkg resolver doesn't like that
-        for julia_version in (v"1.5", v"1.6")
+        for julia_version in (v"1.7.3", v"1.8.0")
             @test_throws Pkg.Resolve.ResolverError collect_artifact_paths([
-                PkgSpec(;name="GMP_jll",  version=v"6.1.2"),
-                PkgSpec(;name="MPFR_jll", version=v"4.1.0"),
+                PkgSpec(;name="OpenBLAS_jll",  version=v"0.3.13"),
+                PkgSpec(;name="libblastrampoline_jll", version=v"5.1.1"),
             ]; platform=Platform("x86_64", "linux"; julia_version))
         end
 
         # So we must pass julia_version == nothing, as is the case in our `linux64` object
         artifact_paths = collect_artifact_paths([
-            PkgSpec(;name="GMP_jll",  version=v"6.1.2"),
-            PkgSpec(;name="MPFR_jll", version=v"4.1.0"),
+            PkgSpec(;name="OpenBLAS_jll",  version=v"0.3.13"),
+            PkgSpec(;name="libblastrampoline_jll", version=v"5.1.1"),
         ]; platform=linux64)
-        @test length(artifact_paths) == 2
-        @test sort([p.name for p in keys(artifact_paths)]) == ["GMP_jll", "MPFR_jll"]
+        @test length(artifact_paths) == 3
+        @test sort([p.name for p in keys(artifact_paths)]) == ["CompilerSupportLibraries_jll", "OpenBLAS_jll", "libblastrampoline_jll"]
     end
 
     # Test adding something that doesn't exist on a certain platform
@@ -119,38 +134,33 @@ if Sys.iswindows()
     exe = ".exe"
 end
 
-@testset "Symlinking" begin
-    # Grab a big JLL, like FFMPEG, symlink them all together, and ensure that all the files exist
-    @testset "FFMPEG" begin
-        artifact_paths = collect_artifact_paths(["FFMPEG_jll"])
+@testset "FFMPEG installation test" begin
+    artifact_paths = collect_artifact_paths(["FFMPEG_jll"])
+    installer_strategies = Dict(
+        "copy" => copy_artifact_paths,
+        "symlink" => symlink_artifact_paths,
+        "hardlink" => hardlink_artifact_paths,
+    )
+    for strategy in keys(installer_strategies)
         mktempdir() do prefix
-            symlink_artifact_paths(prefix, artifact_paths)
+            @testset "$strategy strategy" begin
+                installer_strategies[strategy](prefix, artifact_paths)
 
-            # Ensure that a bunch of tools we expect to be installed are, in fact, installed
-            for tool in ("ffmpeg", "bzcat", "fc-cache", "iconv", "x264", "x265", "xslt-config")
-                @test isfile(joinpath(prefix, "bin", "$(tool)$(exe)"))
-                @test isfile(abspath(joinpath(prefix, "bin", "$(tool)$(exe)")))
+                # Ensure that a bunch of tools we expect to be installed are, in fact, installed
+                for tool in ("ffmpeg", "bzcat", "fc-cache", "iconv", "x264", "x265", "xslt-config")
+                    # Use `@eval` here so the test itself shows the tool name, for easier debugging
+                    tool_name = string(tool, exe)
+                    @eval @test isfile(joinpath($(prefix), "bin", $(tool_name)))
+
+                    # Extra `abspath()` here to explicitly test dereferencing symlinks
+                    @eval @test isfile(abspath(joinpath($(prefix), "bin", $(tool_name))))
+                end
+
+                # Symlinking is insufficient for RPATH, unfortunately.
+                if strategy != "symlink"
+                    run(`$(joinpath(prefix, "bin", "ffmpeg$(exe)")) -version`)
+                end
             end
-        end
-    end
-end
-
-@testset "Copying" begin
-    # Grab a big JLL, like FFMPEG, symlink them all together, and ensure that all the files exist
-    # Also, this should allow us to actually run `ffmpeg`!
-    @testset "FFMPEG" begin
-        artifact_paths = collect_artifact_paths(["FFMPEG_jll"])
-        mktempdir() do prefix
-            copy_artifact_paths(prefix, artifact_paths)
-
-            # Ensure that a bunch of tools we expect to be installed are, in fact, installed
-            for tool in ("ffmpeg", "bzcat", "fc-cache", "iconv", "x264", "x265", "xslt-config")
-                # Use `eval` so that the test failure shows which tools fail
-                tool_name = string(tool, exe)
-                @eval @test isfile(joinpath($(prefix), "bin", $(tool_name)))
-            end
-
-            run(`$(joinpath(prefix, "bin", "ffmpeg$(exe)")) -version`)
         end
     end
 end
